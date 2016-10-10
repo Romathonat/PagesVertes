d57e22893d110a8b2c74babbf820acc7ac2cde97
Romathonat <romain.mathonat@gmail.com>
2016-10-10 14:49:29 +0200

    changing functional dependancy

--- checkDF.py
+++ checkDF.py
@@ -14,12 +14,11 @@ def check_espece_implies_genre(espece_implies_genre, datas, row, errors):
     if genre != datas.cell(row,3).value:
         errors.append('{} DF broken: different genre for same espece {}: {} and {}'.format(row+1, datas.cell(row,4).value, genre, datas.cell(row,3).value))
 
-def check_espece_equiv_type(espece_equivalent_type, datas, row, errors):
-    tree_type = espece_equivalent_type.setdefault(datas.cell(row,4).value, datas.cell(row,5).value)
-    espece = espece_equivalent_type.setdefault(datas.cell(row,5).value, datas.cell(row,4).value)
+def check_type_implies_espece(type_implies_espece, datas, row, errors):
+    espece = type_implies_espece.setdefault(datas.cell(row,2).value, datas.cell(row,4).value)
 
-    if espece != datas.cell(row,4).value or tree_type != datas.cell(row,5).value:
-        errors.append('{} equivalence problem between espece and type (look to previous lines)'.format(row+1))
+    if  datas.cell(row,4).value != espece:
+        errors.append('{} DF broken: different espece for same type {}: {} and {}'.format(row+1, datas.cell(row,2).value, genre, datas.cell(row,4).value))
 
 
 def chechDF():
@@ -35,18 +34,21 @@ def chechDF():
 
     genre_implies_type = {}
     espece_implies_genre = {}
-    espece_equivalent_type = {}
+    type_implies_espece = {}
 
     for row in range(datas.nrows):
         #genre_implies_type
-        check_genre_implies_type(genre_implies_type, datas, row, errors)
+        #check_genre_implies_type(genre_implies_type, datas, row, errors)
 
         #espece_implies_genre
-        check_espece_implies_genre(espece_implies_genre, datas, row, errors)
+        #check_espece_implies_genre(espece_implies_genre, datas, row, errors)
 
         #espece <=> type en francais : it is a double DF
-        check_espece_equiv_type(espece_equivalent_type, datas, row, errors)
+        check_type_implies_espece(type_implies_espece, datas, row, errors)
 
     return errors
 
-print(chechDF())
+errors = chechDF()
+
+for line in errors:
+    print(line)
