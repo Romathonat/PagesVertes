056dba0a520004834cf02a6ae56e7297319a0a51
Romathonat <romain.mathonat@gmail.com>
2016-10-29 14:34:53 +0200

    changing format of output json: more simple, we will recreate hashmap on the fly with JS

--- scripts/generateDatabase.py
+++ scripts/generateDatabase.py
@@ -1,14 +1,15 @@
 from sanitize import sanitize
 import json
 import os
+from utils import hashableDict, set_to_json
 
-sanitized_datas, incomplete_data = sanitize(True)
+sanitized_datas, incomplete_data = sanitize(False)
 
 #those are the tables of our model
 #python dict are hashable, very good to gain perfs
-arbre = {}
-nomBinomial = {}
-feuillage = {}
+arbre = set()
+nomBinomial = set()
+feuillage = set()
 
 #we load the gps into the memory
 gps = {}
@@ -31,33 +32,35 @@ for line in sanitized_datas:
     #we do not add this data if we don't have the gps informations
     if gps_cordinate:
         new_feuillage = {'typeArbre': line[5]}
-        feuillage.setdefault(line[5], new_feuillage)
+        feuillage.add(hashableDict(new_feuillage))
 
         #the firt letter must be upper
         genre = line[3][0].upper()+line[3][1:]
 
         new_nomBinomial = {'genre': genre,'espece': line[4], 'nomFrancais':line[2], 'feuillage': line[5],
         'info_francais': line[9], 'url': line[10], 'description': line[11], 'nom_francais_suggere': line[12]}
-        #we add this data only if it not here yet
-        nomBinomial.setdefault(genre+" "+line[4], new_nomBinomial)
+
+        #we add this data only if it is not here yet
+        nomBinomial.add(hashableDict(new_nomBinomial))
 
         new_arbre = {'id': line[1],'hauteur': line[6], 'diametreTronc': line[7], 'diametreCouronne': line[8],\
         'latitude' : tree_latitude, 'longitude': tree_longitude, 'nomBinomial': genre+" "+line[4]}
-        arbre[line[1]] = new_arbre
+        arbre.add(hashableDict(new_arbre))
 
 
 #now we dump datas into json files
 with open(os.path.join(os.path.dirname(__file__), '../data/json/feuillage.json'), 'w') as f:
-    feuillage_json = json.dumps(feuillage)
-    feuillage_string = 'feuillage = {};'.format(feuillage_json)
+    #we get the representation of
+    feuillage_json = set_to_json(feuillage)[1:-1]
+    feuillage_string = 'feuillage = [{}];'.format(feuillage_json)
     f.write(feuillage_string)
 
 with open(os.path.join(os.path.dirname(__file__),'../data/json/nomBinomial.json'), 'w') as f:
-    nomBinomial_json = json.dumps(nomBinomial)
-    nomBinomial_string = 'nomBinomial = {};'.format(nomBinomial_json)
+    nomBinomial_json = set_to_json(nomBinomial)[1:-1]
+    nomBinomial_string = 'nomBinomial = [{}];'.format(nomBinomial_json)
     f.write(nomBinomial_string)
 
 with open(os.path.join(os.path.dirname(__file__),'../data/json/arbre.json'), 'w') as f:
-    arbre_json = json.dumps(arbre)
-    arbre_string = 'arbre = {};'.format(arbre_json)
+    arbre_json = set_to_json(arbre)[1:-1]
+    arbre_string = 'arbre = [{}];'.format(arbre_json)
     f.write(arbre_string)
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,5 @@
 from unidecode import unidecode
+import re
 
 def is_number(s):
     try:
@@ -14,3 +15,14 @@ def normalize(string_to_normalize):
         return unidecode(string_to_normalize).lower()
     else:
         return int(string_to_normalize)
+
+class hashableDict(dict):
+  def __key(self):
+    return tuple((k,self[k]) for k in sorted(self))
+  def __hash__(self):
+    return hash(self.__key())
+  def __eq__(self, other):
+    return self.__key() == other.__key()
+
+def set_to_json(set_to_convert):
+    return re.sub(r'[\']','"',repr(set_to_convert))
