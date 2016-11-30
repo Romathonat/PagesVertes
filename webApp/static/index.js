9c3309309f2711c97720059662936d0b53a2a69a
winternis <nicolas.nativel@insa-lyon.fr>
2016-11-30 20:28:54 +0100

    hover event for list, marker index handling, map.js refactor in class

--- webApp/static/index.js
+++ webApp/static/index.js
@@ -1,50 +1,63 @@
-var treeListTemplate
-var binominaltemplate
+var treeListTemplate;
+var binominaltemplate;
+var searchMap;
 
 //This function join the json to reconstuct one tree data
 function completeInfo(idTree){
     var currentTree = arbre[idTree];
 
     //here we are making the join, if necessary
-    if (typeof currentTree["nomBinominal"] === 'string') {
-        currentBinominalName = nomBinominal[currentTree["nomBinominal"]];
-        currentBinominalName["feuillage"] = feuillage[currentBinominalName["feuillage"]];
-        currentTree["nomBinominal"] = currentBinominalName;
+    if (typeof currentTree.nomBinominal === 'string') {
+        currentBinominalName = nomBinominal[currentTree.nomBinominal];
+        currentBinominalName.feuillage = feuillage[currentBinominalName.feuillage];
+        currentTree.nomBinominal = currentBinominalName;
     }
-    return(currentTree)
+    return(currentTree);
 }
 
 function searchID() {
-    clearMap()
+    searchMap.clearMap();
     let idTree = document.getElementById("idTree").value.toLowerCase();
-    var tree = completeInfo(idTree)
-    setMarker(tree)
+    var tree = completeInfo(idTree);
+    searchMap.setMarker(tree);
 }
 
 function searchBinominalName(binName){
-    clearMap()
-    let temp = []
+    searchMap.clearMap();
+    let temp = [];
     jQuery.each(arbre, function(i, val) {
         if(val.nomBinominal == binName) {
-            let tree = completeInfo(val.id)
-            temp.push(val)
-            setMarker(tree)
-        }})
+            let tree = completeInfo(val.id);
+            temp.push(val);
+            searchMap.setMarker(tree);
+        }});
     var rendered = Mustache.render(treeListTemplate, {trees: temp});
-    $("#treeList").empty()
+    $("#treeList").empty();
     $("#treeList").append(rendered);
+
+    $(".resultListItem").hover(
+        function(){
+            $(this).css("background-color", "blue");
+            searchMap.highlightMarker(arbre[$(this).attr('id')]);
+        },
+        function(){
+            $(this).css("background-color", "transparent");
+            //searchMap.unhighlightMarker(arbre[$(this).attr('id')]);
+    });
 }
 
 $( document ).ready(function() {
     treeListTemplate = $('#treeListTemplate').html();
     binominaltemplate = $('#binominalNameTemplate').html();
 
+    searchMap = new SearchMap("mapId");
+
     $('.ui.dropdown')
     .dropdown({
         onChange: function(dropdownValue, text, $selectedItem) {
-            searchBinominalName(dropdownValue)
+            searchBinominalName(dropdownValue);
         }
-    })
+    });
 
     var rendered = Mustache.render(binominaltemplate, {name: Object.keys(nomBinominal)});
     $("#binominalName-menu").append(rendered);
--- webApp/static/map.js
+++ webApp/static/map.js
@@ -1,45 +1,63 @@
-var treeMapTemplate
-var markers
-var searchMap;
-var greenIcon = L.icon({
-    iconUrl: './static/images/leaf-green.png',
-    shadowUrl: './static/images/leaf-shadow.png',
-    iconSize: [38, 95], // size of the icon
-    shadowSize: [50, 64], // size of the shadow
-    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62], // the same for the shadow
-    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
+class SearchMap {
+    constructor(divId) {
+        this.treeMapTemplate = $('#mapMarkerTemplate').html();
+
+        this.searchMap = L.map(divId, {
+            center: [45.7807286829, 4.85788730701],
+            zoom: 15,
+            maxZoom: 18
+        });
+
+        this.markers = L.markerClusterGroup(); //markerClusterGroup layer object containing the markers
+        this.searchMap.addLayer(this.markers);
+
+        this.markersIndex = {}; //Index that allows finding a marker with tree id
+
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 18,
+            id: 'romathonat.23e4b6a0',
+            accessToken: 'pk.eyJ1Ijoicm9tYXRob25hdCIsImEiOiJjaXZmM2J4M2cwMDM2Mnpxa253cHVkdHA0In0.FU0Ju6sGpWmlm74TEphPbA'
+        }).addTo(this.searchMap);
+
+        this.greenIcon = L.icon({
+            iconUrl: './static/images/leaf-green.png',
+            shadowUrl: './static/images/leaf-shadow.png',
+            iconSize: [38, 95], // size of the icon
+            shadowSize: [50, 64], // size of the shadow
+            iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+            shadowAnchor: [4, 62], // the same for the shadow
+            popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+        });
+
+        this.currentOpenedMarker = {};
+    }
 
 
-function setMarker(currentTree){
-    var marker = L.marker([currentTree['latitude'], currentTree['longitude']], {
-        icon: greenIcon
-    }).addTo(searchMap);
-    var renderedTemplate = Mustache.render(treeMapTemplate, currentTree);
-    markers.addLayer(marker)
+    setMarker(currentTree){
+        let marker = L.marker([currentTree.latitude, currentTree.longitude], {
+            icon: this.greenIcon
+        });
+        let renderedTemplate = Mustache.render(this.treeMapTemplate, currentTree);
+        marker.bindPopup(renderedTemplate);
+        this.markers.addLayer(marker);
+        this.markersIndex[currentTree.id] = marker;
+    }
+
+    highlightMarker(tree){
+        let renderedTemplate = Mustache.render(this.treeMapTemplate, tree);
+        let marker = this.markersIndex[tree.id];
+        let cluster = this.markers.getVisibleParent(marker);
+        cluster.bindPopup(marker._popup._content).openPopup();
+        this.currentOpenedMarker = cluster;
+    }
+
+    unhighlightMarker(tree){
+        this.currentOpenedMarker.closePopup();
+    }
+
+    clearMap(){
+        this.markers.clearLayers();
+        this.markersIndex = {}
+    }
 }
-
-function clearMap(){
-    markers.clearLayers()
-}
-
-$( document ).ready(function() {
-    treeMapTemplate = $('#mapMarkerTemplate').html();
-
-    searchMap = L.map('mapId', {
-        center: [45.7807286829, 4.85788730701],
-        zoom: 15,
-        maxZoom: 25
-    });
-
-    markers = L.markerClusterGroup();
-    searchMap.addLayer(markers)
-
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 18,
-        id: 'romathonat.23e4b6a0',
-        accessToken: 'pk.eyJ1Ijoicm9tYXRob25hdCIsImEiOiJjaXZmM2J4M2cwMDM2Mnpxa253cHVkdHA0In0.FU0Ju6sGpWmlm74TEphPbA'
-    }).addTo(searchMap);
-});
