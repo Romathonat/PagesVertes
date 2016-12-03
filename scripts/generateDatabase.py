31f8bc4c62082274d32ca3a44df22c4c693e5ca8
Romathonat <romain.mathonat@gmail.com>
2016-12-03 22:47:53 +0100

    erase duplicate data

--- scripts/generateDatabase.py
+++ scripts/generateDatabase.py
@@ -1,23 +1,24 @@
 from sanitize import sanitize
 import json
 import os
-from utils import hashableDict
+from utils import hashableDict, hashableDictArbre
 
 PATH_JSON = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'webApp', 'json')
 
 
 def load_gps():
     """
-    Load the gps from arbreGPS.txt
+    Load the gps from arbreGPS.txt. Return the GPS and line we deleted
     """
     gps = {}
+    data_without_GPS = []
     with open(os.path.join(os.path.dirname(__file__), '../data/arbresGPS.txt'), 'r+') as f:
         for line in f:
             line = line.replace(' ', '')
             id_arbre, latitude, longitude = line.split(',')
             longitude = longitude.rstrip()
             gps['ar'+str(id_arbre)] = (latitude, longitude)
-    return gps
+    return (gps, data_without_GPS)
 
 sanitized_data, incomplete_data = sanitize(False)
 
@@ -28,7 +29,7 @@ nomBinominal = set()
 feuillage = set()
 
 # we load the gps into the memory
-gps = load_gps()
+gps, data_without_GPS  = load_gps()
 
 # for each sanitized_data, we format to JSON correctly
 for line in sanitized_data:
@@ -48,7 +49,7 @@ for line in sanitized_data:
         #the firt letter must be upper
         genre = line[3][0].upper()+line[3][1:]
 
-        new_nomBinominal = {'genre': genre,'espece': line[4], 'nomFrancais':line[2], 'feuillage': line[5],
+        new_nomBinominal = {'genre': genre,'espece': line[4], 'nom_vernaculaire':line[2], 'feuillage': line[5],
         'info_francais': line[9], 'genus_page': line[10], 'species_page': line[11]}
 
         #we add this data only if it is not here yet
@@ -56,8 +57,9 @@ for line in sanitized_data:
 
         new_arbre = {'id': line[1],'hauteur': line[6], 'diametreTronc': line[7], 'diametreCouronne': line[8],\
         'latitude' : tree_latitude, 'longitude': tree_longitude, 'nomBinominal': genre+" "+line[4]}
-        arbre.add(hashableDict(new_arbre))
+        arbre.add(hashableDictArbre(new_arbre))
 
+print(len(arbre))
 
 #now we dump datas into json files
 with open(os.path.join(PATH_JSON,'feuillage.json'), 'w') as f:
@@ -72,3 +74,9 @@ with open(os.path.join(PATH_JSON,'nomBinominal.json'), 'w') as f:
 with open(os.path.join(PATH_JSON,'arbre.json'), 'w') as f:
     arbre_json = list(arbre)
     f.write(json.dumps(arbre_json))
+
+with open(os.path.join(PATH_JSON,'incomplete_data.json'), 'w') as f:
+    f.write(json.dumps(incomplete_data))
+
+with open(os.path.join(PATH_JSON,'data_without_GPS.json'), 'w') as f:
+    f.write(json.dumps(data_without_GPS))
--- scripts/utils.py
+++ scripts/utils.py
@@ -17,12 +17,20 @@ def normalize(string_to_normalize):
         return int(string_to_normalize)
 
 class hashableDict(dict):
-  def __key(self):
-    return tuple((k,self[k]) for k in sorted(self))
-  def __hash__(self):
-    return hash(self.__key())
-  def __eq__(self, other):
-    return self.__key() == other.__key()
+    def __key(self):
+        return tuple((k,self[k]) for k in sorted(self))
+    def __hash__(self):
+        return hash(self.__key())
+    def __eq__(self, other):
+        return self.__key() == other.__key()
+
+class hashableDictArbre(hashableDict):
+    # we define the hash as the id so that we don't have two dict with the same id
+    def __hash__(self):
+        return hash(dict(self.items())['id'])
+    def __eq__(self, other):
+        return dict(self.items())['id'] == dict(other.items())['id']
+
 
 def set_to_json(set_to_convert):
     return re.sub(r'[\']','"',repr(set_to_convert))
